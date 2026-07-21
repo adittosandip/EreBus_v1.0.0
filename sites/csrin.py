767962eda72e2305006abe8ae1521f412fc81da8
root <root@sxxdwip>
2026-07-21 22:30:32 +0200

    Use Playwright storage state for CS.RIN login

--- sites/csrin.py
+++ sites/csrin.py
@@ -20,6 +20,7 @@ class CSRIN(BaseSite):
         )
 
         self.context = self.browser.new_context(
+            storage_state="cookies/state.json",
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
         )
 
